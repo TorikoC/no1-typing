057aa0acf0ab9cd894358a6c277e644ffbb7a4ba
torikoc <breakthroughtgw@gmail.com>
2018-12-12 19:19:27 +0800

    dev

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,10 @@
-export default {
-  // server: 'http://localhost:3000',
-  // apiServer: 'http://localhost:3000/api',
-  server: 'http://localhost',
-  apiServer: 'http://localhost/api',
+const devConfig = {
+  server: 'http://localhost:3000',
+  apiServer: 'http://localhost:3000/api',
 };
+const prodConfig = {
+  server: 'http://35.241.200.20',
+  apiServer: 'http://35.241.200.20/api',
+};
+
+export default (process.env.NODE_ENV === 'production' ? prodConfig : devConfig);
