234183d8e427ec54ac34d195bae56489032984b0
torikoc <breakthroughtgw@gmail.com>
2019-01-03 18:11:06 +0800

    dev

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -4,8 +4,8 @@ const devConfig = {
 };
 
 const prodConfig = {
-  server: 'http://35.234.15.82',
-  apiServer: 'http://35.234.15.82',
+  server: 'https://35.234.15.82',
+  apiServer: 'https://35.234.15.82',
 };
 
 export default (process.env.NODE_ENV === 'production' ? prodConfig : devConfig);
